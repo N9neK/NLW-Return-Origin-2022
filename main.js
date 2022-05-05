42f88d6a731e686f646bd9dae239fbd1d627f0cd
N9neK <ll.nine.kouzuki.ll@gmail.com>
2022-05-05 17:48:22 -0300

    Contact, footer, button back to home and whatsapp

--- main.js
+++ main.js
@@ -2,26 +2,49 @@
 // Number = Números
 // Boolean = True || False
 
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+// Gerenciar os diversos Scrolls da page
 function onScroll() {
-  if (scrollY > 40) {
-    navegar.classList.add('scroll')
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+//Mostrar o Nav ao fazer o Scroll
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
   } else {
-    navegar.classList.remove('scroll')
+    navigation.classList.remove('scroll')
   }
 }
 
+//Mostrar o botão 'BackToTop' ao fazer Scroll
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 360) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//Abrir o menu-expanded ao clicar no ícone dos 3 tracinhos
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
+//Fechar o menu-expanded ao clicar
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
+//Efeito dos elementos da página vão aparecerendo a medida que desce a page
 ScrollReveal({
   origin: 'top',
   distance: '40px',
-  duration: 1000
+  duration: 800
 }).reveal(`#home, 
   #home img, 
   #home .stats, 
