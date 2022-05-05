0a0c6b7ff1df7aef127c513d6cf80563f55fee6e
N9neK <ll.nine.kouzuki.ll@gmail.com>
2022-05-05 00:48:02 -0300

    Dia 3: Transições CSS, animação JS, scroll section

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '40px',
+  duration: 1000
+}).reveal(`#home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
